ab3bd76b1e1ad985eb2e0c6eb27616a7232f2584
6031 <dqas1004t@naver.com>
2025-04-07 17:49:27 +0900

    #4-스프링 부트 프로젝트 초기화

--- src/main/java/com/fastcampus/projectboardadmin/FastcampusProjectAdminApplication.java
+++ src/main/java/com/fastcampus/projectboardadmin/FastcampusProjectAdminApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 public class FastcampusProjectAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FastcampusProjectAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusProjectAdminApplication.class, args);
+    }
 
 }
