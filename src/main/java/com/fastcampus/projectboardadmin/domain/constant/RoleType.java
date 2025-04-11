1f5cadc7de697a18d609d6227dd6430196978a8e
6031 <dqas1004t@naver.com>
2025-04-11 17:05:10 +0900

    #5 - 도메인 설계 : 회원 엔티티, 권한

---
+++ src/main/java/com/fastcampus/projectboardadmin/domain/constant/RoleType.java
@@ -0,0 +1,20 @@
+package com.fastcampus.projectboardadmin.domain.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum RoleType {
+    User("ROLE_USER"),
+
+    MANAGER("ROLE_MANAGER"),
+
+    DEVELOPER("ROLE_DEVELOPER"),
+
+    ADMIN("ROLE_ADMIN");
+
+
+    @Getter
+    private final String roleName;
+
+}
